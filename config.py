d981f29a4596fabfef9782e34afafe2cf615edc8
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-12-03 09:07:56 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config(object):
+    LOGGER = False
+    TG_BOT_TOKEN = os.environ.get('TG_BOT_TOKEN', None) 
+    TG_DUMP_CHANNEL = int(os.environ.get("TG_DUMP_CHANNEL", "0"))
+    URL = os.environ.get('URL', "")
+    PORT = int(os.environ.get('PORT', 5000)) 
+    APP_ID = int(os.environ.get("APP_ID", 6))
+    API_HASH = os.environ.get("API_HASH", "")
+    
+class Development(Config):
+    LOGGER = True
